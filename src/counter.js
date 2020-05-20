9586cb404f48cf7cd26fd97b2b3d053fb8af3e21
Davidoumone <david.moneta28@gmail.com>
2020-05-20 15:25:32 +0200

    modifs éfectués

--- src/counter.js
+++ src/counter.js
@@ -3,12 +3,13 @@ import React, { useState } from 'react';
 function Counter() {
   // Déclare une nouvelle variable d'état, qu’on va appeler « count »
   // Déclare une fonction setCount qui permet de modifier la valeur de count
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState("");
 
   return (
     <div>
       <p>Le compteur est  à : {count} </p>
-      <input id="number" type="number" value={count}/>
+      <input type="text" value={count} onChange={(event) => setCount(+ event.target.value)}/>
+      
       <button onClick={() => setCount(count + 1)}>
         +1
       </button>
